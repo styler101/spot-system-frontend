89a03b5b639f697f91c514c97aee6861a1060cf5
Lucas <lucas.santos@mundowap.com.br>
2023-04-18 07:37:07 -0300

    fix: input width

--- src/app/components/Form/Input/index.ts
+++ src/app/components/Form/Input/index.ts
@@ -7,13 +7,14 @@ interface InputProps {
 export const Input = styled.input<InputProps>`
   margin: 0;
   padding: 0;
-  width: 380px;
+  width: 374px;
   height: 42px;
   border-radius: 4px;
   border: 1px solid #c8c8c8;
   margin-top: 8px;
   padding-left: 14px;
   font-size: 15px;
+
   color: #1a1a1d;
 
   ${(props) =>
