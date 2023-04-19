6c2ab57004a14252d133983b0e9b20870d6a5cd3
Lucas <lucas.santos@mundowap.com.br>
2023-04-19 08:03:52 -0300

    feat: add dashboard route into a routes files

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes as Switch, Route, BrowserRouter } from "react-router-dom";
 import { SignIn } from "../pages/SignIn";
+import { Dashboard } from "../pages/Dashboard";
 
 export function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route index element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Switch>
     </BrowserRouter>
   );
