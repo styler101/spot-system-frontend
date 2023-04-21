a9defe8d9171a31cb0aefd6ef541348a6ec1f16b
Lucas <lucas.santos@mundowap.com.br>
2023-04-21 12:44:15 -0300

    feat: initial structuring of records components

--- src/app/pages/New/index.tsx
+++ src/app/pages/New/index.tsx
@@ -1,12 +1,56 @@
 import React from "react";
+import { Input, Button } from "@/app/components";
+import { Container, Wrapper } from "@/app/assets/styles/global";
+import logo from "@/app/assets/images/logo.svg";
 import * as S from "./styles";
 
 export function New() {
   return (
-    <S.Container>
-      <S.Wrapper>
-        <h1> teste</h1>
-      </S.Wrapper>
-    </S.Container>
+    <Container>
+      <img src={logo} alt="logo do projeto" />
+      <Wrapper>
+        <S.Title> Preencha os campos abaixo para criar um spot </S.Title>
+        <S.Form>
+          <label>
+            <strong> Link da Logo do Spot *</strong>
+            <Input
+              error={false}
+              name="company"
+              placeholder="Informe o link da imagem do spot"
+            />
+          </label>
+          <label>
+            <strong> Empresa *</strong>
+            <Input
+              error={false}
+              name="company"
+              placeholder="Nome da companhia"
+            />
+          </label>
+          <label>
+            <strong>
+              Tecnologias* <span>(separadas por vírgula) </span>
+            </strong>
+            <Input
+              error={false}
+              name="company"
+              placeholder="Informe as tecnologias usadas no spot"
+            />
+          </label>
+          <label>
+            <strong>
+              Valor da diária
+              <span> (em branco para GRATUITO) </span>
+            </strong>
+            <Input
+              error={false}
+              name="company"
+              placeholder="Informe o valor da diária"
+            />
+          </label>
+          <Button> Salvar</Button>
+        </S.Form>
+      </Wrapper>
+    </Container>
   );
 }
--- src/app/pages/New/styles.ts
+++ src/app/pages/New/styles.ts
@@ -1,29 +1,30 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  padding: 40px;
+export const Title = styled.h3`
+  width: 100%;
+  font-size: 18px;
+  text-align: center;
+`;
+
+export const Form = styled.form`
+  margin-top: 32px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`;
-export const Wrapper = styled.div`
   width: 100%;
-  max-width: 450px;
-  flex: 1;
-  margin-top: 16px;
-  background-color: #fff;
-  padding: 16px 32px;
-  border-radius: 4px;
-  box-shadow: 5px 5px 5px rgba(150, 150, 150, 0.3);
-  display: flex;
-  flex-direction: column;
 
-  p {
+  label {
     width: 100%;
-    font-size: 18px;
-    line-height: 20px;
-    text-align: center;
     margin: 8px 0;
+
+    input {
+      width: 450px;
+    }
+    strong > span {
+      font-weight: normal;
+      color: #c8c8c8;
+      font-size: 14px;
+    }
   }
 `;
